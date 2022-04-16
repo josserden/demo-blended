948ccb2934679194e763005f40e51c9db905430f
Den <52295780+josserden@users.noreply.github.com>
2022-04-16 22:09:56 +0300

    finish lesson

--- js/task7.js
+++ js/task7.js
@@ -2,4 +2,18 @@
  * Наше завдання - визначити характер зазначеного речення
  */
 
-console.log('Я бачив у місті такого ж представника, як ти!');
+function getSenescenceType(string) {
+  if (string.endsWith('!')) {
+    return console.log('Це окличне речення ');
+  }
+
+  if (string.endsWith('?')) {
+    return console.log('Це питальне речення ');
+  }
+
+  return console.log('Це розповідне речення ');
+}
+
+getSenescenceType('Я бачив у місті такого ж представника, як ти!');
+getSenescenceType('Чи бачив ти у місті такого ж представника, як я?');
+getSenescenceType('Вибач, але я не в курсі хто ти такий...');
